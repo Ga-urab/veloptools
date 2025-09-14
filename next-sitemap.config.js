1ffe896f1139383e756f9559b9b85716e94502bd
Ga-urab <gaurabaryal9@gmail.com>
2025-09-14 16:24:08 +0545

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://veloptools.com',
+  generateRobotsTxt: true, // also creates robots.txt
+  sitemapSize: 5000,
+}
